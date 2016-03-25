631735ddc3b10509071f87780b0c152693f989c0
katebee <whalenka88@gmail.com>
2016-03-25 15:46:47 +0000

    Improve display of ChoiceAdmin and add question filter

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -15,6 +15,14 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'question', 'votes')
+    list_filter = ['question']
+    search_fields = ['choice_text']
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
